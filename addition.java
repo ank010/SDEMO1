2dd9ca40782f261a67de6cdc927c0cb7d003b060
ank010 <ank010@ucsd.edu>
2022-02-04 01:08:14 -0800

    Write tests

--- addition.java
+++ addition.java
@@ -1,3 +1,15 @@
 public class addition{
+    int a;
+    int b;
 
+    addition(int a, int b){
+        this.a = a;
+        this.b = b;
+    }
+
+    public int MultiplyAdd(int c, int d){
+        int newValue1 = c*a;
+        int newValue2 = d*b;
+        return newValue1 + newValue2;
+    }
 }
--- additionTester.java
+++ additionTester.java
@@ -1,3 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
 public class additionTester{
-  
+  addition Apple = new addition(2,3);
+
+  @Test
+  public void MultiplyAddTest(){
+      assertEquals(100, Apple.MultiplyAdd(5, 6));
+  }
 }
